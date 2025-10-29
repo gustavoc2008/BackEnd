aff34c6b1acf357ec0fdb83caa90c9c63b28f09f
Gustavo <costagustavo2110@gmail.com>
2025-10-29 13:09:58 -0300

    Exercicio Banco

---
+++ CadastroDealunos/Program.cs
@@ -0,0 +1,69 @@
+﻿string[] nomes = new string[3];
+int[] idades = new int[3];
+int totalAlunos = 0;
+
+int opcao = -1;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine($"=== Aplicativo Sala de Aula ===");
+    Console.WriteLine($"1) Listar Alunos");
+    Console.WriteLine($"2) Cadastrar Alunos");
+    Console.WriteLine($"0) Sair");
+    Console.WriteLine($"Escolha uma opcao:");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 0:
+            Console.WriteLine($"Encerrando...");
+            break;
+        case 1:
+            ListarAluno();
+            break;
+        case 2:
+            CadastrarAluno();
+            break;
+        default:
+            Console.WriteLine($"Opcao Invalida");
+            break;
+
+    }
+
+    Console.WriteLine($"Pessione <Enter> para continuar");
+    Console.ReadLine();
+    
+} while (opcao != 0);
+
+void ListarAluno()
+{
+    Console.WriteLine($"=== Listagem de Alunos ===");
+    Console.WriteLine();
+
+    for (int i = 0; i < totalAlunos; i++)
+    {
+        Console.WriteLine($"Nome: {nomes[i]}, {idades[i]} Anos");
+    }
+    
+}
+
+void CadastrarAluno()
+{
+    Console.WriteLine($"=== Cadastro de Alunos ===");
+
+    if (totalAlunos >= 3)
+    {
+        Console.WriteLine($"Limite de vagas atingido");
+        return;
+    }
+    
+    Console.WriteLine($"Digite o nome do aluno:");
+    nomes[totalAlunos] = Console.ReadLine();
+
+    Console.WriteLine($"Digite a idade de {nomes[totalAlunos]}");
+    idades[totalAlunos] = int.Parse(Console.ReadLine());
+    totalAlunos++;
+    Console.WriteLine($"Aluno cadastrado com sucesso!");
+    
+}
---
+++ ReviewArrays/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string[] nomes = new string[4]; //vai de 0 a 3 - o 0 conta
+
+nomes[0] = "Paulo";
+nomes[1] = "Rawany";
+nomes[2] = "Julio";
+
+Console.WriteLine($"Tamnho do Array: {nomes.Length}");
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"{i + 1}ª Nome: {nomes[i]}");
+}
+
